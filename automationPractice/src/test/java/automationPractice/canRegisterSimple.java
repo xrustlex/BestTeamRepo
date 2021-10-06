8b1123a992b96592791e9e39aad088b3405db7e7
vadim <vadim@vadims-mbp.lan>
2021-10-05 21:05:24 -0400

    111

---
+++ automationPractice/src/test/java/automationPractice/canRegisterSimple.java
@@ -0,0 +1,13 @@
+package automationPractice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class canRegisterSimple {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
