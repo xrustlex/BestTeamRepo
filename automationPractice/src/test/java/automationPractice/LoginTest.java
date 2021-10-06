7f68d15ce8eab3d4d1887e6bd9bc41a0166feb38
vadim <vadim@vadims-mbp.lan>
2021-10-05 21:49:14 -0400

    WebDrivers created

--- automationPractice/src/test/java/automationPractice/LoginTest.java
+++ automationPractice/src/test/java/automationPractice/LoginTest.java
@@ -2,16 +2,32 @@ package automationPractice;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class LoginTest {
 
-	//EASY STUFF. WE WILL WIN THIS!
-
-
 	@Test
-
 	public void canLoginTest() {
-
+		
+		File file = new File("src/test/resources/chromedriver");
+		String absolutePath = file.getAbsolutePath();
+		System.setProperty("webdriver.chrome.driver", absolutePath);
+		
+		file = new File("src/test/resources/msedgedriver");
+		absolutePath = file.getAbsolutePath();
+		System.setProperty("webdriver.edge.driver", absolutePath);
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.bing.com");
+		driver.navigate().to("https://www.google.com");
+		driver.navigate().to("http://automationpractice.com");
+		driver.close();
+		
 	}
 }
