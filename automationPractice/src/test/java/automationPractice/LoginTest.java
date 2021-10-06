ad37736938bb72af7fae91031fc4070d2cdb6c56
vadim <vadim@vadims-mbp.lan>
2021-10-05 20:36:54 -0400

    initial start

---
+++ automationPractice/src/test/java/automationPractice/LoginTest.java
@@ -0,0 +1,14 @@
+package automationPractice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
