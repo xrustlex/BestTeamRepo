62ce9be23954c362885b7500167f20e33f877e6a
Zareena Pathan <zareenapat@gmail.com>
2021-10-06 17:02:34 -0700

    Fix the project

--- automationPractice/src/test/java/automationPractice/LoginTest.java
+++ automationPractice/src/test/java/automationPractice/LoginTest.java
@@ -1,9 +1,5 @@
 package automationPractice;
 
-import static org.junit.Assert.*;
-
-import java.io.File;
-
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
