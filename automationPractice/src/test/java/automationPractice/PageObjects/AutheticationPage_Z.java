eed4628fdd7d8542cf2ccafe4e7eeaa26cb624f4
Zareena Pathan <zareenapat@gmail.com>
2021-10-07 13:51:36 -0700

    forgot password commit

---
+++ automationPractice/src/test/java/automationPractice/PageObjects/AutheticationPage_Z.java
@@ -0,0 +1,26 @@
+package automationPractice.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import automationPractice.PageObject;
+
+public class AutheticationPage_Z extends PageObject{
+
+	@FindBy(how=How.CSS, using="a[title='Recover your forgotten password']")
+	private WebElement forgotPasswordLink;
+	
+	public AutheticationPage_Z(WebDriver driver, String baseUrl) {
+		super(driver, baseUrl);
+		
+	}
+
+	public ForgotPasswordPage clickForgotPasswordLink() {
+		forgotPasswordLink.click();
+		return new ForgotPasswordPage(driver,baseUrl);
+		
+	}
+
+}
