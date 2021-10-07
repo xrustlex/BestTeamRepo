49e6d71776ae75c8aab9d4582f68fa5e2b339c3b
vadim <vadim@vadims-mbp.lan>
2021-10-06 20:29:55 -0400

    renames to DriverManager

--- automationPractice/src/test/java/automationPractice/DriverManager.java
+++ automationPractice/src/test/java/automationPractice/DriverManager.java
@@ -4,8 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-public class CreateDriver {
-	public WebDriver createDriver(String browser) {
+public class DriverManager {
+	
+	public WebDriver getDriver(String browser) {
 		if (browser.equals("chrome")) 	return new ChromeDriver();
 		if (browser.equals("edge")) 	return new EdgeDriver();
 		if (browser.equals("safari")) 	return new ChromeDriver();
