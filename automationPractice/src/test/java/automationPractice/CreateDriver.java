0be18f2fd7c84ccaf81acd037e047bc0389a1e7e
vadim <vadim@vadims-mbp.lan>
2021-10-06 20:28:38 -0400

    adds CreateDriver class

---
+++ automationPractice/src/test/java/automationPractice/CreateDriver.java
@@ -0,0 +1,16 @@
+package automationPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class CreateDriver {
+	public WebDriver createDriver(String browser) {
+		if (browser.equals("chrome")) 	return new ChromeDriver();
+		if (browser.equals("edge")) 	return new EdgeDriver();
+		if (browser.equals("safari")) 	return new ChromeDriver();
+
+		return null;
+		
+	}
+}
