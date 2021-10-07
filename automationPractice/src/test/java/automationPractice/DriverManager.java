fe8577f598d205a54a1be82cbc4ea8a7d18d55c2
vadim <vadim@vadims-mbp.lan>
2021-10-06 21:15:29 -0400

    chrome test done

--- automationPractice/src/test/java/automationPractice/DriverManager.java
+++ automationPractice/src/test/java/automationPractice/DriverManager.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class DriverManager {
 	
 	public WebDriver getDriver(String browser) {
+		
 		if (browser.equals("chrome")) 	return new ChromeDriver();
 		if (browser.equals("edge")) 	return new EdgeDriver();
 		if (browser.equals("safari")) 	return new ChromeDriver();
--- automationPractice/src/test/java/automationPractice/LoginTest.java
+++ automationPractice/src/test/java/automationPractice/LoginTest.java
@@ -1,26 +1,46 @@
 package automationPractice;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
+import junit.framework.Assert;
 
 public class LoginTest {
 
 	@Test
 	public void canLoginTest() {
 		
+		String testEmail = "alexanderjosh009@gmail.com";
+		String testPaswd = "monster90";
 		String projectPath = System.getProperty("user.dir");		
 		System.setProperty("webdriver.chrome.driver", projectPath + "/src/test/resources/mac/chromedriver");	
 		System.setProperty("webdriver.edge.driver", projectPath + "/src/test/resources/mac/msedgedriver");
 		System.setProperty("webdriver.safari.driver", projectPath + "/src/test/resources/mac/msedgedriver");
 
 		WebDriver driver = new ChromeDriver();
+		
 		driver.manage().window().maximize();
 		driver.get("http://automationpractice.com");
+		
+		WebElement signInLink = driver.findElement(By.cssSelector("a[class='login']"));
+		signInLink.click();
+		WebElement emailBox = driver.findElement(By.cssSelector("body[id='authentication']"));
+		emailBox.sendKeys(testEmail);
+		WebElement passwordBox = driver.findElement(By.cssSelector("input[class='is_required validate account_input form-control']"));
+		passwordBox.sendKeys(testPaswd);
+		passwordBox.sendKeys(Keys.ENTER);
+		
+		Assert.assertEquals(driver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=my-account");
+		
 		driver.close();
 		
+		/*
 		driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://automationpractice.com");
@@ -30,6 +50,6 @@ public class LoginTest {
 		driver.manage().window().maximize();
 		driver.get("http://automationpractice.com");
 		driver.close();
-		
+		*/
 	}
 }
